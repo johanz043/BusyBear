1d7e7926220e80f6456a4e5653e80dbd0532fb18
Johanz <jomedelte@gmail.com>
2026-06-29 21:22:38 +1000

    Frontend Init

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+function Dashboard(){
+
+    return (
+
+        <div>
+
+            <h1>
+                BusyBear Dashboard
+            </h1>
+
+            <p>
+                Tasks will appear here
+            </p>
+
+        </div>
+
+    )
+
+}
+
+
+export default Dashboard;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+function Login(){
+
+    return (
+        <div>
+
+            <h1>
+                BusyBear Login
+            </h1>
+
+            <p>
+                Frontend is working 🚀
+            </p>
+
+        </div>
+    )
+
+}
+
+
+export default Login;
